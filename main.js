8f9147a54998235ac04f2b73c2333cdc9e60148b
RaymondC181 <99200228+RaymondC181@users.noreply.github.com>
2022-12-19 14:02:52 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 function check()
 {
-    if(bugOne.src == "img/bug1.png")
+    if(bug1.src.match("img/bug1.png"))
     {
-        bugOne.src ="img/squashed.png";
+        bug1.src ="img/squashed.png";
     }
+    
 }
 
 function initialize()
 {
-    bugOne = document.getElementById("bug1");
+    bug1 = document.getElementById("bug1");
+    bug2 = document.getElementById("bug2");
+    bug3 = document.getElementById("bug3");
+    bug4 = document.getElementById("bug4");
+    bug5 = document.getElementById("bug5");
+    bug6 = document.getElementById("bug6");
+    bug7 = document.getElementById("bug7");
+    bug8 = document.getElementById("bug8");
+    bug9 = document.getElementById("bug9");
+    bug10 = document.getElementById("bug10");
+    bug11 = document.getElementById("bug11");
+    bug12 = document.getElementById("bug12");
+    bug13 = document.getElementById("bug13");
+    bug14 = document.getElementById("bug14");
+
 }
